37767750ef79280557736ba9a72baa6254fa72ec
Takayuki Nakanishi <nakanishi@ignote.jp>
2020-03-07 22:34:52 +0900

    amazon商品ページ以外で、アイコンをグレー表示に

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 var openSakura = () => {
 	// urlから商品IDを取得
 	chrome.tabs.getSelected((tab) => {
-		if (tab.url.startsWith('https://www.amazon.co.jp/')) {
-			const itemID = getItemID(tab.url);
-			if (itemID) {
-				const sakuraUrl = `https://sakura-checker.jp/search/${itemID}/`;
-				window.open(sakuraUrl, 'sakuraChecker');
-			}
+		const itemID = getItemID(tab.url);
+		if (itemID) {
+			const sakuraUrl = `https://sakura-checker.jp/search/${itemID}/`;
+			window.open(sakuraUrl, 'sakuraChecker');
 		}
 	});
 };
 
 var getItemID = (url) => {
+	if (!url.startsWith('https://www.amazon.co.jp/')) {
+		return null;
+	}
+
 	const elements = url.split('/');
 	const dpIndex = elements.indexOf('dp');
 	if (dpIndex >= 0 && elements.length > dpIndex) {
@@ -21,6 +23,33 @@ var getItemID = (url) => {
 	}
 };
 
+chrome.tabs.onSelectionChanged.addListener(function(tabId) {
+	chrome.tabs.getSelected((tab) => {
+		// alert(tab.url);
+		const itemID = getItemID(tab.url);
+		if (itemID) {
+			chrome.browserAction.setIcon({
+				path: "icons/icon_16.png",
+				tabId: tab.id
+			});
+		} else {
+			chrome.browserAction.setIcon({
+				path: "icons/icon_disabled_16.png",
+				tabId: tab.id
+			});
+		}
+	});
+});
+
+
 (function () {
-	chrome.browserAction.onClicked.addListener(openSakura);
+	if (chrome.browserAction) {
+		chrome.browserAction.onClicked.addListener(openSakura);
+	}
+
+	if (chrome.tabs || true) {
+		chrome.tabs.onActivated.addListener((info) => {
+			console.log(info);
+		});
+	}
 })();
